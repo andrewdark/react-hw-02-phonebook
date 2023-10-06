a64da4bcc7723bbfd50fd16261cb021add2c9d65
andrewdark <samsonov.a@ukr.net>
2023-10-06 13:08:55 +0300

    add add/find methods to find out some contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,40 @@ import {nanoid} from "nanoid";
 class App extends Component {
   state = {
     contacts: [],
-    filter: [],
+    filter: '',
+  };
+
+  addContact = contact => {
+    const contacts = this.state.contacts;
+    const names = contacts.map(elem => elem.name.toLowerCase());
+    if (names.includes(contact.name.toLowerCase())) {
+      window.alert('The name ' + contact.name + ' already exists');
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: contact.name,
+      number: contact.number,
+    };
+
+    contacts.push(newContact);
+    this.setState({ contacts: contacts });
+  };
+
+  findContact = () => {
+    const filter = this.state.filter;
+    const contacts = this.state.contacts;
+    return contacts.filter(el => {
+      const curName = el.name;
+      let temp = curName.substr(0, filter.length);
+      return filter.toLowerCase() === temp.toLowerCase();
+    });
   };
 
   render() {
     return (
-      <div id={nanoid()}>
+      <div>
         React homework template
       </div>
     );
